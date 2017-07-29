8c12de09813d9a1f59834d6b8a7e141c2fbf6734
Nicholas Whittaker <NickelOz@users.noreply.github.com>
2017-07-29 21:05:00 +1000

    Migrated scraper.py and icon_scraper.py

--- dota2ps.py
+++ dota2ps.py
@@ -26,6 +26,62 @@ class D2Patch:
             print('There was an error in retreiving the blog post, please try again.')
         return None
 
+    def fetch_icons(self):
+        if 'icons' not in os.listdir(os.getcwd()):
+            os.mkdir('icons')
+        self.fetch_hero_icons()
+        self.fetch_item_icons()
+        return None
+
+    def fetch_hero_icons(self):
+        # dota2.gamepedia is used here because of it keeps hero names updated
+        # for example "Necrolyte" --> "Necrophos"
+        r = requests.get('https://dota2.gamepedia.com/Heroes')
+        if r.status_code == 200:
+            try:
+                heroDocument = BeautifulSoup(r.text, 'html.parser')
+                # icons are stored in classed tables within the main page content
+                print('Processing ', end='')
+                for table in heroDocument.find(id='mw-content-text').find_all(class_='wikitable'):
+                    for img in table.find_all('img'):
+                        heroURL = img['src']
+                        heroName = re.search('/[^/]*\?', heroURL).group()[7:-10]
+                        heroName = heroName.replace('%27', '\'').lower()
+                        print(heroName, end=', ', flush=True)
+                        if '{}.png'.format(heroName) not in os.listdir('icons'):
+                            urllib.request.urlretrieve(heroURL, 'icons\\{}.png'.format(heroName))
+                print('DONE!\nAll hero images downloaded successfully')
+            except:
+                print('Unable to parse document, please try again')
+        else:
+            print('Unable to obtain document from {}, status code'.format(r.url, r.status_code))
+        return None
+    
+    def fetch_item_icons(self):
+        # the official dota2 page is used here, as dota2.gamepedia generates
+        #   the item page at execution instead of generating it server-side
+        # some items also have outdated names, this will be fixed in future
+        r = requests.get('https://www.dota2.com/items/')
+        if r.status_code == 200:
+            try:
+                itemDocument = BeautifulSoup(r.text, 'html.parser')
+                print('Processing', end=' ')
+                for column in itemDocument.find_all(class_='shopColumn'):
+                    # this additional loop ignores the column's header icon, which is the only image not encapsulated within a div
+                    for itemContainer in column.find_all(name='div'):
+                        itemURL = itemContainer.find(name='img')['src']
+                        itemName = re.search('/[^/]*\.png', itemURL).group()[1:-7]
+                        print(itemName, end=', ', flush=True)
+                        if '{}.png'.format(itemName) not in os.listdir('icons'):
+                            urllib.request.urlretrieve(itemURL, 'icons\\{}.png'.format(itemName))
+                print('DONE!\nAll item images downloaded successfully')
+            except:
+                print('Unable to parse document, please try again')
+        else:
+            print('Unable to obtain document from {}, status code'.format(r.url, r.status_code))
+        return None
+        
+
     def parse_post_document(self, document):
         post = document.find('div', id='mainLoop').find_all('div')[0]
         post_date, post_content = post.find_all('div')
@@ -38,7 +94,6 @@ class D2Patch:
 
     def parse_post_details(self, raw_date, raw_contents):
         post_date = datetime.strptime(raw_date.string.strip(), '%B %d, %Y - Valve')
-        print('REMOVE' + raw_date.string)
         post_content = list(raw_contents.stripped_strings)
         post_id = post_content[0][:-1]  # remove trailing colon
         post_changelog = post_content[2:]
@@ -94,3 +149,4 @@ class D2Patch:
 if __name__ == '__main__':
     patch = D2Patch()
     patch.fetch_new_post(input('Enter URL of blog post\n>>> '))
+    patch.fetch_icons()
